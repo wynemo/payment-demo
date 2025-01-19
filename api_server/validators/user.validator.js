02306b8a431b52dd9c5eff606018dab61bc72068
wynemo <ggdabin@gmail.com>
2025-01-19 15:49:38 +0800

    move use create method

--- api_server/validators/user.validator.js
+++ api_server/validators/user.validator.js
@@ -4,9 +4,21 @@ import Validator from "./validator.js";
 const { User } = models;
 
 class UserValidator extends Validator {
-    constructor() {
-        super(User);
-    }
+  constructor() {
+    super(User);
+  }
+
+  create() {
+    const schema = this.base();
+
+    // Add any additional validation rules for the create operation here.
+    return schema.keys({
+      first_name: schema.extract("first_name").required(),
+      last_name: schema.extract("last_name").required(),
+      email: schema.extract("email").required().email(),
+      password: schema.extract("password").required(),
+    });
+  }
 }
 
 export default UserValidator;
